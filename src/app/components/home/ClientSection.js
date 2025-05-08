64db4cfaee2ef1ea19a006cf742091477ca2e378
Arjun Suthar <arjunsuthar3427@gmail.com>
2025-05-08 14:13:56 +0530

    Updated  - 2

---
+++ src/app/components/home/ClientSection.js
@@ -0,0 +1,19 @@
+'use client';
+
+import Experience from './experience/index'
+import Skills from './skills/index'
+import Projects from './projects/index'
+import Education from './education/index'
+
+const ClientSection = () => {
+	return (
+		<>
+			<Experience />
+			<Skills />
+			<Projects />
+			<Education />
+		</>
+	)
+}
+
+export default ClientSection
